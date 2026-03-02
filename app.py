19aa7f7cc0341286e9aa4ba61f76531c595dccd9
starkadams08 <mrstarkadams22@gmail.com>
2026-03-02 15:39:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def login():
 
         #return "Invalid credentials"
 
-    return render_template("login.html" error="Invalid credentials")
+    return render_template("login.html")
 
 # ---------------- LOGOUT ---------------- #
 
@@ -111,4 +111,5 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
 
    
+
 
